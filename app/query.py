140129723b8c0dbc8112dc64b52be240e796a4c6
Gonçalo Vinagre Martins <gv.martins@campus.fct.unl.pt>
2024-06-19 10:06:00 +0100

    Some advancements, not tested

--- app/query.py
+++ app/query.py
@@ -1,15 +1,14 @@
-from collections import OrderedDict
 import datetime
 import json
 import os
 import re
-
-from .embeddings import embeddings_processing
+from collections import OrderedDict
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, redirect, render_template, request, session, url_for
 )
 
+from .embeddings import embeddings_processing
 from .opensearch.opensearch import LGPOpenSearch
 
 # Define the paths for the frames and annotations
@@ -32,6 +31,7 @@ def query():
     """Query for a video"""
     if request.method == "POST":
         query_input = request.form["query"]
+        session['query_input'] = query_input
         selected_field = int(request.form.get('field'))
         session['search_mode'] = request.form.get('mode')
         error = None
@@ -44,15 +44,15 @@ def query():
         else:
 
             if selected_field == 1:  # Base Frames Embeddings
-                query_frames_embeddings(query_input)
+                session['query_results'] = query_frames_embeddings(query_input)
             elif selected_field == 2:  # Average Frames Embeddings
-                query_average_frames_embeddings(query_input)
+                session['query_results'] = query_average_frames_embeddings(query_input)
             elif selected_field == 3:  # Best Frame Embeddings
-                query_best_frame_embedding(query_input)
+                session['query_results'] = query_best_frame_embedding(query_input)
             elif selected_field == 4:  # Annotations Embeddings
-                query_annotations_embeddings(query_input)
-            elif selected_field == 5:  # True Expression / Ground Truth
-                query_true_expression(query_input)
+                session['query_results'] = query_annotations_embeddings(query_input)
+            else:  # True Expression / Ground Truth
+                session['query_results'] = query_true_expression(query_input)
 
             return redirect(url_for("query.videos_results"))
 
@@ -97,31 +97,40 @@ def videos_results():
         else:
             return redirect(url_for("query.clips_results", video=selected_video))
 
-    return render_template("query/videos_results.html", frames=frames, videos_info=videos_info, search_mode=search_mode)
+    return render_template("query/videos_results.html", frames=frames, videos_info=videos_info,
+                           search_mode=search_mode)
 
 
 @bp.route("/clips_results/<video>", methods=("GET", "POST"))
 def clips_results(video):
     """ Display the video segments of one video that contain the query results """
     query_results = session.get('query_results')[video]
+    query_input = session.get('query_input')
     search_mode = session.get('search_mode', 1)
 
     # Sort the query results by similarity score
-    query_results = OrderedDict(sorted(query_results.items(), key=lambda x: x[1]['similarity_score'], reverse=True))
+    # query_results = OrderedDict(sorted(query_results.items(), key=lambda x: x[1]['similarity_score'], reverse=True))
 
     frames = {}
-    frames_info = {}
+    # frames_info = {}
 
     # Collect information about the retrieved video segments
     for annotation_id in query_results:
         frames_path = os.path.join(FRAMES_PATH, search_mode, video, annotation_id)
         frames[annotation_id] = os.listdir(frames_path)
-        frames_info[annotation_id] = query_results[annotation_id]
+
         converted_start_time = str(datetime.timedelta(seconds=int(query_results[annotation_id]["start_time"]) // 1000))
-        frames_info[annotation_id]["converted_start_time"] = converted_start_time
         converted_end_time = str(datetime.timedelta(seconds=int(query_results[annotation_id]["end_time"]) // 1000))
+
+        query_results[annotation_id]["converted_start_time"] = converted_start_time
+        query_results[annotation_id]["converted_end_time"] = converted_end_time
+
+        """
+        frames_info[annotation_id] = query_results[annotation_id]
+        frames_info[annotation_id]["converted_start_time"] = converted_start_time
         frames_info[annotation_id]["converted_end_time"] = converted_end_time
         frames_info[annotation_id]["similarity_score"] = query_results[annotation_id]["similarity_score"]
+        frames_info[annotation_id]["avg_embedding"] = query_results[annotation_id]["avg_embedding"]"""
 
     if search_mode == FACIAL_EXPRESSIONS_ID:
         # Not all frames of each expression are going to be displayed
@@ -144,7 +153,7 @@ def clips_results(video):
             frames_to_display[expression] = frames_to_display[expression][:num_frames_to_display]
 
         return render_template("query/clips_results/expressions_clips.html", frames=frames_to_display,
-                               frames_info=frames_info,
+                               frames_info=query_results, query_input=query_input,
                                search_mode=search_mode, video=video)
     elif search_mode == PHRASES_ID:
 
@@ -152,39 +161,89 @@ def clips_results(video):
         frames_to_display = frames
 
         return render_template("query/clips_results/phrases_clips.html", frames=frames_to_display,
-                               frames_info=frames_info,
+                               frames_info=query_results, query_input=query_input,
                                search_mode=search_mode, video=video)
+
+
+def thesaurus_results(clip_avg_embedding):
+    """ Display the thesaurus results for a similar sign """
+    search_results = query_thesaurus(clip_avg_embedding)
+    search_mode = session.get('search_mode', 1)
+
+    frames = {}
+
+    # Collect information about the retrieved video segments
+    for video_id in search_results:
+        for annotation_id in search_results[video_id]:
+            frames_path = os.path.join(FRAMES_PATH, search_mode, video_id, annotation_id)
+            frames[video_id][annotation_id] = os.listdir(frames_path)
+
+            converted_start_time = str(
+                datetime.timedelta(seconds=int(search_results[annotation_id]["start_time"]) // 1000))
+            converted_end_time = str(datetime.timedelta(seconds=int(search_results[annotation_id]["end_time"]) // 1000))
+
+            search_results[annotation_id]["converted_start_time"] = converted_start_time
+            search_results[annotation_id]["converted_end_time"] = converted_end_time
+
+    # Not all frames of each expression are going to be displayed
+    frames_to_display = {}
+    num_frames_to_display = 6
+
+    # Display only #num_frames_to_display frames of each expression
+    for video_id in frames:
+        for expression, all_frames in frames[video_id].items():
+
+            # Calculate the step size
+            if len(all_frames) <= num_frames_to_display:
+                step_size = 1
+            else:
+                step_size = (len(all_frames) - 1) // num_frames_to_display + 1
+
+            # Select frames to display
+            frames_to_display[expression] = all_frames[::step_size]
+
+            # Ensure that only num_frames_to_display frames are selected
+            frames_to_display[expression] = frames_to_display[expression][:num_frames_to_display]
+
+    return render_template("query/clips_results/thesaurus_clips.html", frames=frames,
+                           frames_info=search_results, search_mode=search_mode)
 
 
 def query_frames_embeddings(query_input):
     """ Get the results of the query using the frames embeddings """
     query_embedding = embeddings_processing.generate_query_embeddings(query_input, embedder)
     search_results = opensearch.knn_query(query_embedding.tolist(), N_RESULTS)
-    set_query_results(search_results, query_input)
+    return set_query_results(search_results, query_input)
 
 
 def query_average_frames_embeddings(query_input):
     """ Get the results of the query using the average of the frames embeddings """
     query_embedding = embeddings_processing.generate_query_embeddings(query_input, embedder)
     search_results = opensearch.knn_query_average(query_embedding.tolist(), N_RESULTS)
-    set_query_results(search_results, query_input)
+    return set_query_results(search_results, query_input)
 
 
 def query_best_frame_embedding(query_input):
     """ Get the results of the query using the best frame embedding """
     query_embedding = embeddings_processing.generate_query_embeddings(query_input, embedder)
     search_results = opensearch.knn_query_best(query_embedding.tolist(), N_RESULTS)
-    set_query_results(search_results, query_input)
+    return set_query_results(search_results, query_input)
 
 
 def query_annotations_embeddings(query_input):
     """ Get the results of the query using the annotations embeddings """
     query_embedding = embeddings_processing.generate_query_embeddings(query_input, embedder)
     search_results = opensearch.knn_query_annotations(query_embedding.tolist(), N_RESULTS)
-    set_query_results(search_results, query_input)
+    return set_query_results(search_results, query_input)
 
 
-def set_query_results(search_results, query_input):
+def query_thesaurus(clip_avg_embedding):
+    """ Get the results of querying for a similar sign """
+    search_results = opensearch.knn_query_average(clip_avg_embedding, N_RESULTS)
+    return set_query_results(search_results)
+
+
+def set_query_results(search_results, query_input=None):
     """ Set the info of the query from the search results """
     query_results = {}
 
@@ -200,12 +259,13 @@ def set_query_results(search_results, query_input):
             'end_time']
         query_results[hit['_source']['video_id']][hit['_source']['annotation_id']]['phrase'] = hit['_source']['phrase']
         query_results[hit['_source']['video_id']][hit['_source']['annotation_id']]['similarity_score'] = hit['_score']
+        query_results[hit['_source']['video_id']][hit['_source']['annotation_id']]['avg_embedding'] = hit['_source'][
+            'average_frame_embedding']
 
-    print_performance_metrics(query_results, query_input)
+    if query_input:
+        print_performance_metrics(query_results, query_input)
 
-    # Convert the dictionary to a list of tuples and store it in the session
-    session['query_results'] = query_results
-    print(session['query_results'])
+    return query_results
 
 
 def query_true_expression(query_input):
@@ -229,8 +289,6 @@ def query_true_expression(query_input):
                         if search_mode == FACIAL_EXPRESSIONS_ID:
                             query_results[video][annotation["annotation_id"]]['phrase'] = annotation["phrase"]
                         query_results[video][annotation["annotation_id"]]['similarity_score'] = "N/A"
-
-    session['query_results'] = query_results
 
     return query_results
 
