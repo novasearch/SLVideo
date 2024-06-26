367c8086e8eccc8656a0d790f266318b351c4812
Gonçalo Vinagre Martins <57198766+GoncaloZen@users.noreply.github.com>
2024-06-26 15:40:58 +0100

    Added success message and redirect to previous route

--- app/annotations.py
+++ app/annotations.py
@@ -2,18 +2,27 @@ import json
 import os
 import datetime
 
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, flash, url_for
 
 from app.constants import ANNOTATIONS_PATH, FACIAL_EXPRESSIONS_ID
 
 bp = Blueprint('annotations', __name__)
 
+prev_page = None
+
 
 @bp.route("/edit_annotation/<video_id>/<annotation_id>", methods=("GET", "POST"))
 def edit_annotation(video_id, annotation_id):
     """ Edit an annotation """
+    global prev_page
 
-    prev_page = request.headers.get('Referer', None)
+    current_route = url_for('annotations.edit_annotation', video_id=video_id, annotation_id=annotation_id,
+                            _external=True)
+    referer = request.headers.get('Referer', None)
+
+    if (referer.replace('http://', '').replace('https://', '') !=
+            current_route.replace('http://', '').replace('https://', '')):
+        prev_page = referer
 
     with open(os.path.join(ANNOTATIONS_PATH, f"{video_id}.json"), "r") as f:
         video_annotations = json.load(f)
@@ -28,9 +37,32 @@ def edit_annotation(video_id, annotation_id):
             phrase = annotation["phrase"]
 
     converted_start_time = (
-                datetime.datetime.min + datetime.timedelta(seconds=int(start_time) // 1000)).time().strftime("%H:%M:%S")
+            datetime.datetime.min + datetime.timedelta(seconds=int(start_time) // 1000)).time().strftime("%H:%M:%S")
     converted_end_time = (datetime.datetime.min + datetime.timedelta(seconds=int(end_time) // 1000)).time().strftime(
         "%H:%M:%S")
+
+    if request.method == "POST":
+        # data = request.form
+        # expression = data["expression"]
+        # start_time = data["start_time"]
+        # end_time = data["end_time"]
+        # phrase = data["phrase"]
+        #
+        # for annotation in video_annotations[FACIAL_EXPRESSIONS_ID]["annotations"]:
+        #     if annotation["annotation_id"] == annotation_id:
+        #         annotation["value"] = expression
+        #         annotation["start_time"] = int(start_time)
+        #         annotation["end_time"] = int(end_time)
+        #         annotation["phrase"] = phrase
+        #
+        # with open(os.path.join(ANNOTATIONS_PATH, f"{video_id}.json"), "w") as f:
+        #     json.dump(video_annotations, f, indent=4)
+
+        flash("Annotation updated successfully!", "success")
+
+        return render_template("annotations/edit_annotation.html", video=video_id, annotation_id=annotation_id,
+                               prev_page=prev_page, expression=expression, start_time=converted_start_time,
+                               end_time=converted_end_time, phrase=phrase)
 
     return render_template("annotations/edit_annotation.html", video=video_id, annotation_id=annotation_id,
                            prev_page=prev_page, expression=expression, start_time=converted_start_time,
